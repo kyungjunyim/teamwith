a99f6e8a2567cf64564ee00baa90d4aa7bfccf7f
Hwang kyujin <https://github.com/HWANGKYUJIN>
2018-08-18 21:45:24 +0900

    0818 20:00 itg

--- src/main/java/com/teamwith/controller/TeamInfoController.java
+++ src/main/java/com/teamwith/controller/TeamInfoController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,7 +25,6 @@ import com.teamwith.vo.TeamSimpleVO;
 
 @Controller
 @RequestMapping(value="/teamInfo")
-
 public class TeamInfoController {
 	@Inject
 	private TeamService teamService;
@@ -31,19 +32,19 @@ public class TeamInfoController {
 	private ApplicationService applicationService;
 	
 	@RequestMapping(value="/register",method=RequestMethod.GET)
-	public String registerTeamView() {
+	public String registerTeamView(Model model) {
 		return "teambuilding/jsp/teamRegister";
 	}
 	
 	@RequestMapping(value="/register",method=RequestMethod.POST)
-	public String registerTeam(HttpSession session,TeamDetailVO teamInfo) {
-//		teamService.registerTeam(teamInfo, file);
+	public String registerTeam(HttpSession session,TeamDetailVO teamInfo,String[] faqQuestions,String[] faqAnswers,String[] interviewQuestionContents,String[] skill1,String[] skill2,String[] skill3,String[] recruitPreferencecs,String[] recruitExplains, String[] recruitPeopleNum) {
+		//		teamService.registerTeam(teamInfo, file);
 		return null;
 	}
 	
 	@RequestMapping(value="/applicant/{teamId}",method=RequestMethod.GET)
 	public String showApplicant(@PathVariable("teamId") String teamId,Model model) {
-		String key="team_id-"+teamId;
+		String key="team-"+teamId;
 		List<ApplicantVO> applicantList=applicationService.getApplicant(key);
 		
 		Map<String, List<InterviewVO>> interviewMap = null;
@@ -51,7 +52,7 @@ public class TeamInfoController {
 			interviewMap = new HashMap<String, List<InterviewVO>>();
 			for(ApplicantVO applicant : applicantList) {
 				interviewMap.put(applicant.getApplicationId(), applicationService.getMyInterview(applicant.getApplicationId()));
-			}			
+			}
 		}
 		
 		TeamSimpleVO teamInfo = null;
@@ -64,6 +65,6 @@ public class TeamInfoController {
 		model.addAttribute("applicantList",applicantList);
 		model.addAttribute("interviewMap",interviewMap);
 		
-		return "teambuilding/jsp/myApplicant.jsp";
+		return "teambuilding/jsp/myApplicant";
 	}
 }
