0842b20318586d9a5226d6d1f46d44daad66eab9
kyungjunyim <killop2000@gmail.com>
2018-08-18 16:11:29 +0900

    test

---
+++ src/main/java/com/teamwith/service/TeamService.java
@@ -0,0 +1,370 @@
+package com.teamwith.service;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.servlet.http.Part;
+
+import org.springframework.stereotype.Service;
+
+import com.teamwith.dao.CommentDAO;
+import com.teamwith.dao.FaqDAO;
+import com.teamwith.dao.RecruitDAO;
+import com.teamwith.dao.RequireSkillDAO;
+import com.teamwith.dao.TeamDAO;
+import com.teamwith.dao.TeamDetailDAO;
+import com.teamwith.dao.TeamSimpleDAO;
+import com.teamwith.dao.TeamSkillDAO;
+import com.teamwith.dto.CommentDTO;
+import com.teamwith.dto.FaqDTO;
+import com.teamwith.dto.RecruitDTO;
+import com.teamwith.dto.RequireSkillDTO;
+import com.teamwith.dto.TeamDTO;
+import com.teamwith.dto.TeamSkillDTO;
+import com.teamwith.util.Criteria;
+import com.teamwith.util.UploadFileUtils;
+import com.teamwith.vo.CommentVO;
+import com.teamwith.vo.FaqVO;
+import com.teamwith.vo.RecruitVO;
+import com.teamwith.vo.RequireSkillVO;
+import com.teamwith.vo.TeamDetailVO;
+import com.teamwith.vo.TeamSimpleVO;
+import com.teamwith.vo.TeamSkillVO;
+
+@Service
+public class TeamService {
+	@Inject
+	private FaqDAO faqDAO;
+	@Inject
+	private TeamSimpleDAO teamSimpleDAO;
+	@Inject
+	private RequireSkillDAO requireSkillDAO;
+	@Inject
+	private RecruitDAO recruitDAO;
+	@Inject
+	private CommentDAO commentDAO;
+	@Inject
+	private TeamDAO teamDAO;
+	@Inject
+	private TeamSkillDAO teamSkillDAO;
+	@Inject
+	private TeamDetailDAO teamDetailDAO;
+
+	public List<FaqVO> getFaq(String teamId) throws Exception {
+		List<FaqDTO> result = faqDAO.searchFaqByTeamId(teamId);
+		List<FaqVO> faq = new ArrayList<FaqVO>();
+
+		for (FaqDTO faqDTO : result) {
+			faq.add(faqDTO.toVO());
+		}
+		return faq;
+	}
+
+	public List<TeamSimpleVO> getMyTeam(Criteria cri, String memberId) throws Exception {
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("memberId", memberId);
+		paramMap.put("page", cri.getPage());
+		paramMap.put("perPageNum", cri.getPerPageNum());
+		List<TeamSimpleVO> result = teamSimpleDAO.searchTeamSimpleAllByMemberId(paramMap);
+
+		return result;
+	}
+
+	public TeamSimpleVO getTeamSimple(String teamId) throws Exception {
+		TeamSimpleVO result = teamSimpleDAO.searchTeamSimple(teamId);
+		return result;
+	}
+
+	public List<TeamSimpleVO> getJoinedTeam(Criteria cri) throws Exception {
+		List<TeamSimpleVO> result = teamSimpleDAO.searchTeamSimpleAllByTeamId(cri);
+
+		return result;
+	}
+
+	public List<RequireSkillVO> getRequireSkills(String recruitId) throws Exception {
+		List<RequireSkillDTO> list = requireSkillDAO.searchRequireSkillByRecruitId(recruitId);
+		List<RequireSkillVO> result = new ArrayList<RequireSkillVO>();
+
+		for (RequireSkillDTO obj : list) {
+			result.add(obj.toVO());
+		}
+
+		return result;
+	}
+
+	public List<RequireSkillVO> getRequireSkillsByTeamId(String teamId) throws Exception {
+		List<RequireSkillDTO> list = requireSkillDAO.searchRequireSkillByRecruitId(teamId);
+		List<RequireSkillVO> result = new ArrayList<RequireSkillVO>();
+
+		for (RequireSkillDTO obj : list) {
+			result.add(obj.toVO());
+		}
+
+		return result;
+	}
+
+	public List<RecruitVO> getRecruitInfo(String teamId) throws Exception {
+		List<RecruitDTO> list = recruitDAO.searchRecruitByTeamId(teamId);
+		List<RecruitVO> result = new ArrayList<RecruitVO>();
+
+		for (RecruitDTO obj : list) {
+			result.add(obj.toVO());
+		}
+
+		return result;
+	}
+
+	public int registerComment(CommentVO comment) throws Exception {
+		List<String> commentId = commentDAO.getId();
+
+		String generatedId = generateId(commentId, "comment");
+		comment.setCommentId(generatedId);
+		int result = commentDAO.addComment(comment.toDTO());
+
+		return result;
+	}
+
+	public int removeCommnet(String commentId) throws Exception {
+		int result = commentDAO.removeCommentByCommentId(commentId);
+
+		return result;
+	}
+
+	public int updateComment(String commentId, String commentContent) throws Exception {
+		Map<String, String> paramMap = new HashMap<String, String>();
+		paramMap.put("commentId", commentId);
+		paramMap.put("commentContent", commentContent);
+
+		int result = commentDAO.updateCommentContent(paramMap);
+
+		return result;
+	}
+
+	public List<CommentVO> getComments(String teamId) throws Exception {
+		List<CommentDTO> list = commentDAO.searchCommentByTeamId(teamId);
+		List<CommentVO> result = new ArrayList<CommentVO>();
+
+		for (CommentDTO obj : list) {
+			result.add(obj.toVO());
+		}
+
+		return result;
+	}
+
+	public List<TeamSimpleVO> getRecentTeam(Criteria cri) throws Exception {
+		List<TeamSimpleVO> result = teamSimpleDAO.searchRecentTeamSimple(cri);
+
+		return result;
+	}
+
+	public List<String> searchTeam(Criteria cri) throws Exception {
+		List<String> result = new ArrayList<String>();
+
+		List<TeamDTO> list = teamDAO.searchTeamByCondition(cri);
+
+		for (TeamDTO obj : list) {
+			result.add(obj.getTeamId());
+		}
+
+		return result;
+	}
+
+	public List<String> searchTeamByRoleSkill(Criteria cri) throws Exception {
+		List<String> result = new ArrayList<String>();
+
+		List<TeamSkillVO> list = teamSkillDAO.searchTeamByRoleSkill(cri);
+
+		for (TeamSkillVO obj : list) {
+			result.add(obj.getTeamId());
+		}
+
+		return result;
+	}
+
+	public List<String> searchTeamDTO(Criteria cri) throws Exception {
+		List<String> result = new ArrayList<String>();
+
+		List<TeamSkillDTO> list = teamSkillDAO.searchTeamDTOByRoleSkill(cri);
+		
+		for (TeamSkillDTO obj : list) {
+			result.add(obj.getTeamId());
+		}
+
+		return result;
+	}
+
+	public List<TeamSimpleVO> getTeamListByTeamId(Criteria cri) throws Exception {
+		List<TeamSimpleVO> result = teamSimpleDAO.searchTeamSimpleAllByTeamId(cri);
+
+		return result;
+	}
+
+	public int registerFaq(List<FaqVO> faq) throws Exception {
+		int result = 0;
+		
+		for (FaqVO obj : faq) {
+			List<String> faqId = faqDAO.getId();
+
+			String generatedId = generateId(faqId, "faq");
+			obj.setFaqId(generatedId);
+
+			result += faqDAO.addFaq(obj.toDTO());
+		}
+
+		return result;
+	}
+
+	public List<String> registerRecruit(List<RecruitVO> recruit) throws Exception {
+		List<String> result = new ArrayList<String>();
+
+		for (RecruitVO obj : recruit) {
+			List<String> recruitId = recruitDAO.getId();
+
+			String genereatedId = generateId(recruitId, "recruit");
+			obj.setRecruitId(genereatedId);
+			result.add(genereatedId);
+
+			recruitDAO.addRecruit(obj.toDTO());
+		}
+
+		return result;
+	}
+
+	public int registerRequireSkill(RequireSkillVO requireSkill) throws Exception {
+		int result = 0;
+
+		for (RequireSkillDTO obj : requireSkill.toDTO()) {
+			result += requireSkillDAO.addRequireSkill(obj);
+		}
+
+		return result;
+	}
+
+	public String registerTeam(TeamDetailVO teamInfo, Part file) throws Exception {
+		List<String> teamId = teamDAO.getId();
+		String generatedId = generateId(teamId, "team");
+
+		TeamDTO team = new TeamDTO();
+
+		team.setTeamId(generatedId);
+		team.setTeamProjectName(teamInfo.getTeamProjectName());
+		team.setTeamName(teamInfo.getTeamName());
+		team.setTeamSummary(teamInfo.getTeamSummary());
+		team.setTeamContent(teamInfo.getTeamContent());
+		team.setProjectCategoryId(teamInfo.getProjectCategoryId());
+		team.setRegionId(teamInfo.getRegionId());
+		team.setTeamEndDate(Date.valueOf(teamInfo.getTeamEndDate()));
+		team.setTeamContestName(teamInfo.getTeamContestName());
+		team.setTeamContestLink(teamInfo.getTeamContestLink());
+		team.setMemberId(teamInfo.getMemberId());
+
+		String teamPicPath = UploadFileUtils.uploadFile("c:/teamwith/image/team", generatedId, file);
+		team.setTeamPic(teamPicPath);
+
+		teamDAO.addTeam(team);
+
+		return generatedId;
+	}
+
+	public int removeTeam(String teamId) throws Exception {
+		int result = teamDAO.removeTeamByTeamId(teamId);
+
+		return result;
+	}
+
+	public TeamDetailVO getTeamInfo(String teamId) throws Exception {
+		TeamDetailVO result = teamDetailDAO.searchTeamDetailByTeamId(teamId);
+
+		return result;
+	}
+
+	public int updateFaq(List<FaqVO> faq) throws Exception {
+		int result = 0;
+
+		for (FaqVO obj : faq) {
+			result += faqDAO.updateFaq(obj.toDTO());
+		}
+
+		return result;
+	}
+
+	public List<String> updateRecruit(List<RecruitVO> recruit) throws Exception {
+		List<String> result = new ArrayList<String>();
+
+		for (RecruitVO obj : recruit) {
+			recruitDAO.updateRecruit(obj.toDTO());
+			result.add(obj.getRecruitId());
+		}
+
+		return result;
+	}
+
+	public List<RequireSkillDTO> removeRequireSkill(RequireSkillVO requireSkill) throws Exception {
+		List<RequireSkillDTO> result = new ArrayList<RequireSkillDTO>();
+
+		for (RequireSkillDTO obj : requireSkill.toDTO()) {
+			requireSkillDAO.removeRequireSkill(obj);
+			result.add(obj);
+		}
+
+		return result;
+	}
+
+	public int updateRequireSkill(RequireSkillVO requireSkill) throws Exception {
+		return registerRequireSkill(requireSkill);
+	}
+
+	public String updateTeam(TeamDetailVO teamInfo, Part file) throws Exception {
+		TeamDTO team = new TeamDTO();
+
+		team.setTeamId(teamInfo.getTeamId());
+		team.setTeamProjectName(teamInfo.getTeamProjectName());
+		team.setTeamName(teamInfo.getTeamName());
+		team.setTeamSummary(teamInfo.getTeamSummary());
+		team.setTeamContent(teamInfo.getTeamContent());
+		team.setProjectCategoryId(teamInfo.getProjectCategoryId());
+		team.setRegionId(teamInfo.getRegionId());
+		team.setTeamEndDate(Date.valueOf(teamInfo.getTeamEndDate()));
+		team.setTeamPic(teamInfo.getTeamPic());
+		team.setTeamContestName(teamInfo.getTeamContestName());
+		team.setTeamContestLink(teamInfo.getTeamContestLink());
+		team.setMemberId(teamInfo.getMemberId());
+
+		UploadFileUtils.uploadFile("c:/teamwith/image/team", teamInfo.getTeamId(), file);
+
+		teamDAO.updateTeam(team);
+
+		return team.getTeamId();
+	}
+
+	public int changeTeamStatus(int status, String teamId) throws Exception {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("status", status);
+		map.put("teamId", teamId);
+
+		int result = teamDAO.updateTeamStatus(map);
+
+		return result;
+	}
+
+	private String generateId(List<String> id, String tableName) {
+		if (id == null) {
+			return tableName + "-" + 1;
+		}
+
+		int maxCnt = -1;
+
+		for (String str : id) {
+			if (maxCnt < Integer.parseInt(str.split("-")[1])) {
+				maxCnt = Integer.parseInt(str.split("-")[1]);
+			}
+		}
+
+		return tableName + "-" + (maxCnt + 1);
+	}
+
+}
