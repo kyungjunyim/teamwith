db380534b1808a3b708e000e0960c451f9488f48
lana <ygj02054@gmail.com>
2018-08-18 17:29:05 +0900

    Controllers

---
+++ src/main/java/com/teamwith/controller/PologMainController.java
@@ -0,0 +1,70 @@
+package com.teamwith.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.teamwith.service.MemberService;
+import com.teamwith.service.ProfileService;
+import com.teamwith.vo.MemberPraiseCntVO;
+import com.teamwith.vo.MemberSimpleVO;
+import com.teamwith.vo.MemberSkillVO;
+import com.teamwith.vo.MemberTendencyVO;
+import com.teamwith.vo.MemberVO;
+
+@Controller
+public class PologMainController {
+
+	private static final Logger logger = LoggerFactory.getLogger(PologMainController.class);
+
+	@Inject
+	ProfileService profileService;
+	@Inject
+	MemberService memberService;
+
+	@RequestMapping(value = "/{memberId}", method = RequestMethod.GET)
+	public String home(@PathVariable(value = "memberId") String memberId, Model model, HttpSession session) {
+		String sessionMemId = null;
+		MemberSimpleVO msVO = (MemberSimpleVO) session.getAttribute("memberSimpleVO");
+		if (msVO != null) {
+			sessionMemId = msVO.getMemberId();
+		}
+		try {
+			MemberVO mem = profileService.getMemberInfo(memberId);
+			/* 비공개 회원이면 */
+			if (mem == null) {
+				/* 비공개지만 본인이 요청하면 정보 보여줌 */
+				if (memberId.equals(sessionMemId)) {
+					mem = profileService.getMyInfo(memberId);
+				} else {
+					return "private";
+				}
+			}
+
+			List<MemberPraiseCntVO> result = memberService.getMemberPraiseCnt(memberId);
+			List<String> projectCategory = memberService.getMemberProjectCategory(memberId);
+			MemberSkillVO skill = memberService.getMemberSkill(memberId);
+			MemberTendencyVO tendency = profileService.getMemberTendency(memberId);
+
+			model.addAttribute("memberVO", mem);
+			model.addAttribute("memberProjectCategoryList", projectCategory);
+			model.addAttribute("praiseCntList", result);
+			model.addAttribute("skillVO", skill);
+			model.addAttribute("tendencyVO", tendency);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "polog/jsp/pologMain";
+	}
+
+}
