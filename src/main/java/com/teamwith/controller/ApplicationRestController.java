967e6943677edc73b070fb9ce173c0634f53d98e
lana <ygj02054@gmail.com>
2018-08-25 20:35:08 +0900

    A

--- src/main/java/com/teamwith/controller/ApplicationRestController.java
+++ src/main/java/com/teamwith/controller/ApplicationRestController.java
@@ -8,11 +8,10 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teamwith.service.ApplicationService;
@@ -21,7 +20,6 @@ import com.teamwith.vo.InterviewVO;
 import com.teamwith.vo.MemberSimpleVO;
 import com.teamwith.vo.MyApplicationVO;
 
-
 @RestController
 @RequestMapping("/api/application")
 public class ApplicationRestController {
@@ -29,6 +27,31 @@ public class ApplicationRestController {
 	private ApplicationService applicationService;
 	@Inject
 	private MemberSimpleVO memberSimpleVO;
+
+	@ResponseBody
+	@RequestMapping(value = "/myApplication", method = RequestMethod.GET)
+	public Map<String, Object> myApplication(HttpSession session) {
+		memberSimpleVO = (MemberSimpleVO) session.getAttribute("memberSimpleVO");
+		String memberId = memberSimpleVO.getMemberId();
+		System.out.println(memberId);
+		// String memberId = "yim";
+		Map<String, Object> result = new HashMap<String, Object>();
+
+		// 나의 지원 목록 가져오기
+		List<MyApplicationVO> myApplicationList = applicationService.getMyApplication(memberId);
+		result.put("myApplicationList", myApplicationList);
+
+		// 지원 별로 인터뷰 목록 가져오기
+		Map<String, List<InterviewVO>> interviewMap = new HashMap<String, List<InterviewVO>>();
+		for (MyApplicationVO myApplication : myApplicationList) {
+			List<InterviewVO> interviewVOList = applicationService.getMyInterview(myApplication.getApplicationId());
+			interviewMap.put(myApplication.getApplicationId(), interviewVOList);
+		}
+		result.put("interviewMap", interviewMap);
+
+		return result;
+
+	}
 
 	@RequestMapping(value = "/apply/{teamId}", method = RequestMethod.POST)
 	public Boolean apply(@PathVariable("teamId") String teamId, HttpSession session, String[] interviewAnswer,
