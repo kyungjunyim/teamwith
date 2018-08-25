abb6ed8f648a57b19f6ec7a43e93bcb67499b561
lana <ygj02054@gmail.com>
2018-08-25 11:56:33 +0900

    portfolio rest controller

--- src/main/java/com/teamwith/controller/LoginRestController.java
+++ src/main/java/com/teamwith/controller/LoginRestController.java
@@ -21,15 +21,14 @@ public class LoginRestController {
 	private LoginService loginService;
 	@Inject
 	private MemberSimpleVO memberSimpleVO;
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public Map<String, Object> login(@RequestBody Map<String, String> param, HttpSession session) throws Exception {
 		String sessionId = session.getId();
-		System.out.println(sessionId);
 		Map<String, Object> result = new HashMap<String, Object>();
 		memberSimpleVO = loginService.login(param.get("memberId"), param.get("memberPassword"));
-		
-		if(memberSimpleVO == null) {
+
+		if (memberSimpleVO == null) {
 			result.put("result", "false");
 		} else {
 			session.setAttribute("memberSimpleVO", memberSimpleVO);
@@ -37,6 +36,12 @@ public class LoginRestController {
 			result.put("memberSimpleVO", memberSimpleVO);
 		}
 		return result;
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public void logout(HttpSession session) {
+		// 세션 무효화
+		session.invalidate();
 	}
 
 }
